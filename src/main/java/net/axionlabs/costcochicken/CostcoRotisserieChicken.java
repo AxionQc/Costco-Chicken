3309a77eafaf507e2c2d3c2ee693838482470275
Noah <noahlemay09@hotmail.com>
2024-09-16 19:08:44 -0500

    Prepare

---
+++ src/main/java/net/axionlabs/costcochicken/CostcoRotisserieChicken.java
@@ -0,0 +1,10 @@
+package net.axionlabs.costcochicken;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CostcoRotisserieChicken implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // Initialization code
+    }
+}
